278f6f9413e2963524f9fac704a3b0c452030abf
mohammad jamshidia <mohammad_jamshida@yahoo.com>
2023-11-11 15:23:40 +0330

    use async metod(arrow function) to counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  
-  const increaseHandler = () =>  setCounter(counter + 1);
-  const decreaseHandler = () =>  setCounter(counter - 1);
-  
-  
+
+  const increaseHandler = () => {
+    setCounter((counter)=> counter + 1);
+  }
+  const increaseHandler3 = () => {
+    setCounter((counter)=> counter + 3);
+  }
+  const stopHandler = () => {
+    setCounter(0);}
+
   return (
     <div>
       <h1>Counter</h1>
       <p>{counter}</p>
-      <button onClick={increaseHandler}>+</button>
-      <button onClick={decreaseHandler}>-</button>
+      <button onClick={stopHandler}>stop</button>
+      <button onClick={increaseHandler}>1+</button>
+      <button onClick={increaseHandler3}>3+</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
